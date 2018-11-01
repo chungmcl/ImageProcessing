6bc529477fb4c4ab02c9423ea9099f22a936ab81
Micheal Chung <chungmcl@hotmail.com>
2018-11-01 14:19:11 -0700

    Issue with structure

--- ImageProcessingCPlusPlus/ImageProcessingCPlusPlusHeaderFile.h
+++ ImageProcessingCPlusPlus/ImageProcessingCPlusPlusHeaderFile.h
@@ -1,3 +1,13 @@
 #pragma once
 
-int GetEncoderClsid(const WCHAR* format, CLSID* pClsid);
+#ifndef ImageProcessing
+#define ImageProcessing
+class ImageProcessing
+{
+private:
+	int GetEncoderClsid(const WCHAR* format, CLSID* pClsid);
+	void Initialize(const WCHAR* location);
+public:
+
+};
+#endif
