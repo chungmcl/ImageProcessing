cde1d1d473f96a47e6e7721017f8dc9e121592e3
Micheal Chung <chungmcl@hotmail.com>
2018-11-20 14:11:54 -0800

    Rotated image

--- ImageProcessingCPlusPlus/ImageProcessingCPlusPlusHeaderFile.h
+++ ImageProcessingCPlusPlus/ImageProcessingCPlusPlusHeaderFile.h
@@ -5,13 +5,13 @@ class ImageProcessor
 private:
 	Gdiplus::Bitmap currentImage;
 	Gdiplus::BitmapData BMD;
-	Gdiplus::Rect GetRekt();
-	void Rotate();
-	byte* GetPixel(int x, int y);
-	void EndProcess();
+	Gdiplus::Rect GetRekt(Gdiplus::Bitmap &image, Gdiplus::BitmapData &data);
+	byte* GetPixel(int x, int y, Gdiplus::BitmapData BMD);
+	void EndProcess(Gdiplus::Bitmap &image, Gdiplus::BitmapData &BMD);
 	int GetEncoderClsid(const WCHAR* format, CLSID* pClsid);
 public:
 	ImageProcessor(const WCHAR* location);
 	void GrayScale();
 	void HorizontalMirror();
+	void Rotate();
 };
