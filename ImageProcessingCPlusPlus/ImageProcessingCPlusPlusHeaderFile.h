7cbbdf72df5cf042957dc294f19c3d8eeabf2de3
Micheal Chung <chungmcl@hotmail.com>
2018-12-04 13:31:08 -0800

    Fixed code

--- ImageProcessingCPlusPlus/ImageProcessingCPlusPlusHeaderFile.h
+++ ImageProcessingCPlusPlus/ImageProcessingCPlusPlusHeaderFile.h
@@ -5,12 +5,13 @@ class ImageProcessor
 private:
 	Gdiplus::Bitmap currentImage;
 	Gdiplus::BitmapData BMD;
+	Gdiplus::PixelFormat pixelFormat;
 	Gdiplus::Rect GetRekt(Gdiplus::Bitmap &image, Gdiplus::BitmapData &data);
 	byte* GetPixel(int x, int y, Gdiplus::BitmapData BMD);
 	void EndProcess(Gdiplus::Bitmap &image, Gdiplus::BitmapData &BMD);
 	int GetEncoderClsid(const WCHAR* format, CLSID* pClsid);
 public:
-	ImageProcessor(const WCHAR* location);
+	ImageProcessor(const WCHAR* location, Gdiplus::PixelFormat pf);
 	void GrayScale();
 	void HorizontalMirror();
 	void Rotate();
