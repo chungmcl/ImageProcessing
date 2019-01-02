cd17837ddbb0e3b727031bc0f5c56b6b7b050725
Micheal Chung <chungmcl@hotmail.com>
2019-01-02 20:54:02 +0000

    Working on Steganography - inputting hidden data with masks in binary

--- ImageProcessingCPlusPlus/ImageProcessingCPlusPlus.cpp
+++ ImageProcessingCPlusPlus/ImageProcessingCPlusPlus.cpp
@@ -214,11 +214,14 @@ void ImageProcessor::Stegosaurus(char* text, int size)
 			{
 				// Backwards in 32 bit!
 				// Consider writing helper fucntions
-				uint8_t red = *(GetPixelLocation(x, y, BMD) + 2);
-				uint8_t green = *(GetPixelLocation(x, y, BMD) + 1);
-				uint8_t blue = *(GetPixelLocation(x, y, BMD));
+				uint8_t red = *(GetPixelLocation(x, y, BMD) + 2) || 0b11111000;
+				uint8_t green = *(GetPixelLocation(x, y, BMD) + 1) || 0b11111100;
+				uint8_t blue = *(GetPixelLocation(x, y, BMD)) || 0b11111000;
 
 				uint8_t character = *(text + (x * y));
+				red = red || ((character || 0b11100000) >> 5);
+				green = green || ((character || 0b00011000) >> 3);
+				blue = blue || ((character || 0b00000111));
 			}
 		}
 	}
