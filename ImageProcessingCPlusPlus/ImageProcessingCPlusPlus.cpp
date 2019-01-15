28ac560beabedf83084e0e6632876194e39d6e53
Micheal Chung <chungmcl@hotmail.com>
2019-01-15 14:14:38 -0800

    working on stegonagraphfiesa

--- ImageProcessingCPlusPlus/ImageProcessingCPlusPlus.cpp
+++ ImageProcessingCPlusPlus/ImageProcessingCPlusPlus.cpp
@@ -224,6 +224,7 @@ uint32_t ImageProcessor::PaccPixel(uint8_t red, uint8_t green, uint8_t blue, uin
 
 void ImageProcessor::Stegosaurus(const char* text, int textLength)
 {
+	// 4 pixels (32 bits) of header data detailing size of text
 	Rect rect = GetRekt(currentImage, BMD);
 	// 3 2 3
 	int pixelCount = BMD.Width * BMD.Height;
@@ -242,17 +243,18 @@ void ImageProcessor::Stegosaurus(const char* text, int textLength)
 				x = 0;
 			}
 
-			uint8_t red = *(GetPixelLocation(x, y, BMD) + 2) | 0b11111000;
-			uint8_t green = *(GetPixelLocation(x, y, BMD) + 1) | 0b11111100;
-			uint8_t blue = *(GetPixelLocation(x, y, BMD)) | 0b11111000;
+			uint8_t red = *(GetPixelLocation(x, y, BMD) + 2) & 0b11111000;
+			uint8_t green = *(GetPixelLocation(x, y, BMD) + 1) & 0b11111100;
+			uint8_t blue = *(GetPixelLocation(x, y, BMD)) & 0b11111000;
+			uint8_t alpha = *(GetPixelLocation(x, y, BMD) + 3);
 
-			uint8_t character = *(text + (x * y));
-			red = red | ((character | 0b11100000) >> 5);
-			green = green | ((character | 0b00011000) >> 3);
-			blue = blue | ((character | 0b00000111));
-			uint8_t setTo = PaccPixel(red, green, blue);
+			uint8_t character = *(text + i);
+			red = red | ((character & 0b11100000) >> 5);
+			green = green | ((character & 0b00011000) >> 3);
+			blue = blue | ((character & 0b00000111));
+			uint32_t setTo = PaccPixel(red, green, blue, alpha);
 
-			*(GetPixelLocation(x, y, BMD)) = setTo;
+			SetPixel(x, y, setTo);
 
 			x++;
 		}
